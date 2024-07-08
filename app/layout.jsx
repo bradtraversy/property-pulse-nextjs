8594385db2ea3beb87ba0d871223432c2bb6a7f5
Brad Traversy <traversymedia@gmail.com>
2024-07-08 12:16:56 -0400

    Initial setup

---
+++ app/layout.jsx
@@ -0,0 +1,19 @@
+import '@/assets/styles/globals.css';
+
+export const metadata = {
+  title: 'PropertyPulse',
+  description: 'Find The Perfect Rental Property',
+  keywords: 'rental, property, real estate',
+};
+
+const MainLayout = ({ children }) => {
+  return (
+    <html lang='en'>
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default MainLayout;
---
+++ app/page.jsx
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return <h1 className='text-2xl'>HomePage</h1>;
+};
+
+export default HomePage;
