7e070a12d0d2894bb830ad156d6a54085280ee58
Brad Traversy <traversymedia@gmail.com>
2024-07-09 10:33:14 -0400

    Homepage components

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,15 @@
 import Link from 'next/link';
+import Hero from '../components/Hero';
+import InfoBoxes from '@/components/InfoBoxes';
+import Footer from '@/components/Footer';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1 className='text-3xl'>Welcome</h1>
-      <Link href='/properties'>Properties</Link>
-    </div>
+    <>
+      <Hero />
+      <InfoBoxes />
+      <Footer />
+    </>
   );
 };
 export default HomePage;
