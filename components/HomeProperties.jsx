26440f41189e3a52f0aee0995d76c600f64096f0
Brad Traversy <traversymedia@gmail.com>
2024-07-09 11:47:05 -0400

    Changes

--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -9,9 +9,9 @@ const HomeProperties = () => {
 
   return (
     <>
-      <section class='px-4 py-6'>
-        <div class='container-xl lg:container m-auto'>
-          <h2 class='text-3xl font-bold text-blue-500 mb-6 text-center'>
+      <section className='px-4 py-6'>
+        <div className='container-xl lg:container m-auto'>
+          <h2 className='text-3xl font-bold text-blue-500 mb-6 text-center'>
             Recent Properties
           </h2>
           <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
