9415e1c5a252d9e161e4d69f753317a0b0e8e94d
Brad Traversy <traversymedia@gmail.com>
2024-07-16 11:56:52 -0400

    Add images to theme property page

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,7 @@ import { GlobalProvider } from '@/context/GlobalContext';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import '@/assets/styles/globals.css';
+import 'photoswipe/dist/photoswipe.css';
 
 export const metadata = {
   title: 'PropertyPulse',
