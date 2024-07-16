7fda092131a1c32df889bc5fcccdc23c5afd64fe
Brad Traversy <traversymedia@gmail.com>
2024-07-16 12:26:12 -0400

    Back button

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -7,6 +7,8 @@ import BookmarkButton from '@/components/BookmarkButton';
 import ShareButtons from '@/components/ShareButtons';
 import PropertyContactForm from '@/components/PropertyContactForm';
 import { convertToSerializeableObject } from '@/utils/convertToObject';
+import Link from 'next/link';
+import { FaArrowLeft } from 'react-icons/fa';
 
 const PropertyPage = async ({ params }) => {
   await connectDB();
@@ -24,6 +26,16 @@ const PropertyPage = async ({ params }) => {
   return (
     <>
       <PropertyHeaderImage image={property.images[0]} />
+      <section>
+        <div className='container m-auto py-6 px-6'>
+          <Link
+            href='/properties'
+            className='text-blue-500 hover:text-blue-600 flex items-center'
+          >
+            <FaArrowLeft className='mr-2' /> Back to Properties
+          </Link>
+        </div>
+      </section>
       <section className='bg-blue-50'>
         <div className='container m-auto py-10 px-6'>
           <div className='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
